30e16b8e1b4d64165aeb411e34facf83ba1cbaaa
Saurav Tamta Sharma <132946215+Saurav-sharmaa@users.noreply.github.com>
2023-10-30 16:21:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from instagrapi import Client
+from instagrapi.types import Usertag, Location
+
+imgdir = "D:\\pythonProject\\images\\"  # add directory of folder
+
+
+def listDir(dir):
+    filename = (os.listdir(dir)[0])
+    global a
+    a = os.path.abspath(os.path.join(dir, filename))
+    return a
+
+
+if __name__ == '__main__':
+    listDir(imgdir)
+
+cl = Client()
+cl.login(username="brimbiii", password="55sept")
+
+media = cl.photo_upload(
+    path=a,
+    caption="random"
+)
